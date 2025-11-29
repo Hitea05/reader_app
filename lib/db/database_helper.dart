0de5d68a77ed60338beb583613a06756b2df0a28
WAI PHYO <hitea0@gmail.com>
2025-11-29 22:37:11 +0630

    add insert and read all book

---
+++ lib/db/database_helper.dart
@@ -0,0 +1,61 @@
+import 'package:path/path.dart';
+import 'package:reader_app/models/book.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static const _databaseName = 'book_database.db';
+  static const _databaseVersion = 1;
+  static const _tableName = 'books';
+
+  DatabaseHelper._privateConstructor();
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+
+  static Database? _database;
+
+  Future<Database> get database async {
+    _database ??= await _initDatabase();
+    return _database!;
+  }
+
+  _initDatabase() async {
+    String path = join(await getDatabasesPath(), _databaseName);
+    return await openDatabase(
+      path,
+      version: _databaseVersion,
+      onCreate: _onCreate,
+    );
+  }
+
+  Future _onCreate(Database db, int version) {
+    return db.execute('''
+    CREATE TABLE $_tableName(
+    id TEXT PRIMARY KEY,
+    title TEXT NOT NULL,
+    authors TEXT NOT NULL,
+    publisher TEXT,
+    publishedDate TEXT,
+    description TEXT,
+    industryIdentifiers TEXT,
+    pageCOunt INTEGER,
+    language TEXT,
+    imageLinks TEXT,
+    previewLink TEXT,
+    infoLink TEXT,
+    subtitle TEXT
+    )
+    ''');
+  }
+
+  Future<int> insert(Book book) async {
+    Database db = await instance.database;
+    return await db.insert(_tableName, book.toJson());
+  }
+
+  Future<List<Book>> readAllBook() async {
+    Database db = await instance.database;
+    var books = await db.query(_tableName);
+    return books.isNotEmpty
+        ? books.map((bookdata) => Book.fromJsonDatabase(bookdata)).toList()
+        : [];
+  }
+}
